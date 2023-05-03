67be29a860c4716495a8265c84373081c5d37c9e
ErionOruglica <eo61125@ubt-uni.net>
2023-05-03 07:32:41 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const sql = require('mssql/msnodesqlv8');
+
+var config = {
+
+       server : "DESKTOP-OKEREVD",
+      database: "Restauranti",
+     options :{
+       trustedConnection:true,
+     },
+    driver:"msnodesqlv8",
+ }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
